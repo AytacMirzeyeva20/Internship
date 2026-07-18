cc361bc961c331ecc792be4ca3598f51ccac751c
Aytac.Mirzeyeva <mirzeyevaa91@gmail.com>
2026-07-18 11:28:50 +0400

    Form Validation tamamlandi

--- Task-1/form.js
+++ Task-1/form.js
@@ -5,6 +5,19 @@ const name=document.getElementById("name").value;
 const gmail=document.getElementById("gmail").value;
 const subject=document.getElementById("subject").value;
 const message=document.getElementById("message").value;
+const gmailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+if (!gmailRegex.test(gmail)) {
+    alert("Please enter your email");
+    return;
+}
+if(name===""){
+    alert("Please enter your name");
+    return;
+}
+if(message===""){
+    alert("Enter message");
+    return;
+}
 console.log("Name",name);
 console.log("Gmail",gmail);
 console.log("Subject",subject);

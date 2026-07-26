ee2bdcc76039ebefbe2dddedb8f9b3f1a812abf3
Aytac.Mirzeyeva <mirzeyevaa91@gmail.com>
2026-07-26 20:36:09 +0400

    Drag-drop finished

--- Task-2/script.js
+++ Task-2/script.js
@@ -1,9 +1,13 @@
-let tasks=[];
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+let dragTask=null;
 const todo=document.getElementById("todo-list");
 const progress=document.getElementById("progress-list");
 const done=document.getElementById("done-list");
 const taskInput = document.getElementById("task-input");
 const addButton = document.getElementById("add-btn");
+function saveTasks(){
+    localStorage.setItem("tasks",JSON.stringify(tasks))
+}
 function renderTasks() {
 todo.textContent="";
 progress.textContent="";
@@ -11,6 +15,10 @@ done.textContent="";
 tasks.forEach(task=>{
 const card = document.createElement("div");
 card.className = "task";
+card.draggable=true;
+card.addEventListener("dragstart",()=>{
+    dragTask=task;
+})
 const title = document.createElement("p");
 title.textContent = task.title;
  const actions = document.createElement("div");
@@ -22,6 +30,7 @@ title.textContent = task.title;
 
  deleteIcon.addEventListener('click',()=>{
     tasks=tasks.filter(item => item.id !== task.id);
+    saveTasks();
     renderTasks()
  })
  editIcon.addEventListener('click',()=>{
@@ -36,7 +45,7 @@ title.textContent = task.title;
     }
 
     task.title = newName.trim();
-
+    saveTasks();
     renderTasks();
  })
 
@@ -44,17 +53,19 @@ title.textContent = task.title;
  actions.appendChild(deleteIcon);
  card.appendChild(actions);
 card.appendChild(title);
-if (task.status === "todo") {
-    todo.appendChild(card);
-} else if (task.status === "progress") {
-    progress.appendChild(card);
-} else if (task.status === "done") {
-    done.appendChild(card);
-}
+
+ if (task.status === "todo") {
+            todo.appendChild(card);
+        } else if (task.status === "progress") {
+            progress.appendChild(card);
+        } else if (task.status === "done") {
+            done.appendChild(card);
+        }
+
 })
 }
-const addbutton=document.getElementById("add-btn");
-addbutton.addEventListener('click',()=>{
+
+addButton.addEventListener('click',()=>{
 const title=taskInput.value.trim();
 if(title===""){
     alert("Metnin ici bosdur.")
@@ -67,6 +78,37 @@ if(title===""){
     });
 
     taskInput.value = "";
+  saveTasks();
+    renderTasks();
+})
+renderTasks();
+todo.addEventListener("dragover", (e) => {
+    e.preventDefault();
+});
 
+progress.addEventListener("dragover", (e) => {
+    e.preventDefault();
+});
+
+done.addEventListener("dragover", (e) => {
+    e.preventDefault();
+});
+todo.addEventListener("drop",()=>{
+      if (!dragTask) return;
+    dragTask.status = "todo";
+    saveTasks();
+    renderTasks();
+})
+
+progress.addEventListener("drop",()=>{
+       if (!dragTask) return;
+    dragTask.status = "progress";
+    saveTasks();
+    renderTasks();
+})
+done.addEventListener("drop",()=>{
+       if (!dragTask) return;
+    dragTask.status = "done";
+    saveTasks();
     renderTasks();
 })

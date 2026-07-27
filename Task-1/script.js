d4b9f2a68ab3b469022c9c70d6cacce8e64edd99
Aytac.Mirzeyeva <mirzeyevaa91@gmail.com>
2026-07-27 12:01:21 +0400

    Task 1 yenilendi

--- Task-1/script.js
+++ Task-1/script.js
@@ -1,5 +1,7 @@
-const menubar=document.getElementById("bar")
-const menu=document.getElementById("nav_item")
-menubar.addEventListener("click",function(){
-    menu.classList.toggle("active")
-})
+const menubar = document.getElementById("bar");
+const menu = document.getElementById("nav_item");
+menubar.addEventListener("click", function () {
+    menu.classList.toggle("active");
+const expanded = menu.classList.contains("active");
+    menubar.setAttribute("aria-expanded", expanded);
+});

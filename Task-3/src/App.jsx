9e1b8547a11241f1b201c39103e49c845021aac7
Aytac.Mirzeyeva <mirzeyevaa91@gmail.com>
2026-08-02 12:34:10 +0400

    SearchBar hazirlandi

--- Task-3/src/App.jsx
+++ Task-3/src/App.jsx
@@ -5,23 +5,36 @@ import ResultList from "./components/ResultList";
 import Pagination from "./components/Pagination";
 
 const API_KEY = "293a085dd6c23c6a4bf35fb6d3c3e1a4";
-
 function App() {
   const [movies, setMovies] = useState([]);
+  const[search,setSearch]=useState("");
+  const [debouncedSearch, setDebouncedSearch] = useState("");
 
-  useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data.results);
-        setMovies(data.results);
-      });
-  }, []);
+   useEffect(() => {
+  const timer = setTimeout(() => {
+    setDebouncedSearch(search);
+  }, 500);
+  return () => clearTimeout(timer);
+}, [search]);
+
+useEffect(() => {
+ const url = debouncedSearch
+    ? `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${debouncedSearch}`
+    : `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      setMovies(data.results || []);
+    });
+}, [debouncedSearch]);
 
   return (
     <>
       <Navbar />
+         <SearchBar search={search} setSearch={setSearch} />
       <ResultList movies={movies} />
+   
     </>
   );
 }
--- Task-3/src/components/SearchBar.jsx
+++ Task-3/src/components/SearchBar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-export default function SearchBar() {
+export default function SearchBar({search,setSearch}) {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className='mt-50'>
+        <h1 className='text-center text-5xl font-bold'>Discover Amazing Movies</h1>
+        <p className='text-center text-[20px] mt-2'> Search thousands of movies instantly.</p>
+      </div>
+      <div className='flex gap-6 justify-center items-center mt-7 mb-18'>
+         <input type="text" placeholder="Search movie..." className='w-96 rounded-full border border-slate-700 bg-slate-900 px-5 py-3 text-white outline-none focus:border-red-500'  value={search}  onChange={(e) => setSearch(e.target.value)}/>
+      <button className='rounded-3xl bg-red-500 px-8 py-3 font-semibold text-white hover:bg-red-600'>Search</button>
+      </div>
+    </>
   )
 }
